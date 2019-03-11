b6a3bc47da1397fe6f1e8f232008f2e3249a9182
Vanessa Chalem <vchalem@salesforce.com>
2019-03-11 16:29:38 -0400

    data setup

---
+++ orgInit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sfdx force:org:create -f config/project-scratch-def.json -a HCADK2 --setdefaultusername -d 1
+
+sfdx force:package:install --package 04t1C000000AoPO -w 20 
+
+sfdx force:mdapi:deploy --deploydir mdapi-source/app-config
+
+sfdx force:mdapi:deploy --deploydir mdapi-source/data-config
+
+sfdx force:mdapi:deploy --deploydir mdapi-source/org-config
+
+sfdx force:source:push 
+
+#sfdx assign permission sets
+
+sfdx force:apex:execute -f create-demo-data-setup.apex
+#sfdx force:apex:execute -f config/create-demo-data.apex
+
+
+sfdx force:org:open
+
